26d041bde6005ac69aa94b4788fe8ec96148d0b7
guyfreund <gfreund@paloaltonetworks.com>
2021-08-07 15:08:10 +0300

    implementation of 8 actions

--- DuelingDQN/main.py
+++ DuelingDQN/main.py
@@ -10,19 +10,19 @@ class DiscreteActionSpaceWrapper(gym.Wrapper):
         action_space = environment.action_space
         assert isinstance(action_space, gym.spaces.Box)
 
-        environment.action_space = gym.spaces.Discrete(4)
+        environment.action_space = gym.spaces.Discrete(8)
         super(DiscreteActionSpaceWrapper, self).__init__(environment)
 
     def step(self, discrete_action):
         continuous_action = {
             0: np.array([-1, 0]),
-            # 1: np.array([0.2, 0]),  # main 60%
-            # 2: np.array([0.6, 0]),  # main 80%
-            1: np.array([1, 0]),  # main 100%
-            # 4: np.array([-1, -0.75]),  # left 75%
-            2: np.array([-1, -1]),  # left 100%
-            # 6: np.array([-1, 0.75]),  # right 75%
-            3: np.array([-1, 1])  # right 100%
+            1: np.array([0.2, 0]),  # main 60%
+            2: np.array([0.6, 0]),  # main 80%
+            3: np.array([1, 0]),  # main 100%
+            4: np.array([-1, -0.75]),  # left 75%
+            5: np.array([-1, -1]),  # left 100%
+            6: np.array([-1, 0.75]),  # right 75%
+            7: np.array([-1, 1])  # right 100%
         }[discrete_action]
 
         obs, reward, done, info = self.envirnoment.step(continuous_action)
@@ -39,7 +39,7 @@ graph = True
 file_type = 'tf'
 file = 'saved_networks/duelingdqn_model123'
 
-duelingdqn_agent = Agent(lr=0.00075, discount_factor=0.99, num_actions=4, epsilon=1.0, batch_size=64, input_dim=[8])
+duelingdqn_agent = Agent(lr=0.00075, discount_factor=0.99, num_actions=8, epsilon=1.0, batch_size=64, input_dim=[8])
 
 if train and not test:
     duelingdqn_agent.train_model(env, num_episodes, graph)
